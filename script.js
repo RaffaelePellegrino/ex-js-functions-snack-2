766517927cda4a268a3aecc8c108cecb2fe5a153
RaffaelePellegrino <raffaele.pellegrino02@gmail.com>
2025-03-17 17:07:37 +0100

    snack 9

--- script.js
+++ script.js
@@ -112,3 +112,18 @@ function contoAllaRovescia (countdown) {
 }
 contoAllaRovescia(5)
 
+// snack 9 (bonus) 
+
+function sequenzaOperazioni(operazioni, intervallo){
+
+    operazioni.forEach((operazione,index) => {
+        setTimeout(() => {
+            operazione()
+        }, intervallo * index);
+    });
+}
+sequenzaOperazioni([
+    () => console.log("Operazione 1"),
+    () => console.log("Operazione 2"),
+    () => console.log("Operazione 3")
+], 1000)
